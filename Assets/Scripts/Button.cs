feeca706fbffaefd3fbf33f3b0cc372970b84ed5
Reinier E <r.evangel@live.com>
2017-08-30 16:45:01 -0700

    initial commit

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+    public Sprite buttonOff;
+    public Sprite buttonPressed;
+    public Sprite buttonOn;
+
+    private SpriteRenderer sr;
+
+	// Use this for initialization
+	void Awake () {
+        sr = GetComponent<SpriteRenderer>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        
+	}
+
+    private void OnMouseDown() {
+        if (sr.sprite == buttonOff) {
+            sr.sprite = buttonPressed;
+        } else if (sr.sprite == buttonOn) {
+            sr.sprite = buttonPressed;
+        }
+    }
+
+    private void OnMouseUp() {
+        if (sr.sprite == buttonPressed) {
+            sr.sprite = buttonOn;
+        }
+    }
+
+
+
+}
