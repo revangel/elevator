89219891d5b34f79d95a3bbf616c1987f08341df
Reinier E <r.evangel@live.com>
2018-05-09 19:30:55 -0400

    initial commit (previously untracked?)

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+    	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OpenDoor() {
+
+    }
+
+    IEnumerator MoveSprite() {
+        yield return null;
+    }
+}
---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+    Camera cam;
+
+    void Start() {
+        cam = GetComponent<Camera>();
+    }
+
+    void Update() {
+        Ray ray = cam.ScreenPointToRay(new Vector3(200, 200, 0));
+        Debug.DrawRay(ray.origin, ray.direction * 10, Color.red);
+    }
+}
